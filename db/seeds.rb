27022f46812e1a596dea70bd4a5a98c6c35d0db9
Jamaleh <Jamaleh@hotmail.com>
2015-06-10 11:54:24 +1000

    Assignment 32 complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 require 'faker'
-
+ 
 50.times do 
   Post.create!(
     title:  Faker::Lorem.sentence,
@@ -7,14 +7,21 @@ require 'faker'
   )
 end
 posts = Post.all
-
+ 
 100.times do
   Comment.create!(
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
-end 
+end
 
+post = Post.where(title: "My special title").first 
+
+unless post
+  Post.create(title: "My special title", body: "Some long body here")
+end
+ 
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
