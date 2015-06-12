ef407f3f4dd525073f6d876e81dfcfa452aed6a8
Jamaleh <Jamaleh@hotmail.com>
2015-06-13 00:10:50 +1000

    progress a 33

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,50 @@
+class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :body))
+    if @question.save
+      flash[:notice] = "Question was saved."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :new
+    end 
+  end 
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    if @question.update_attributes(params.require(:question).permit(:title, :body))
+      flash[:notice] = "Question was updated."
+      redirect_to @question
+    else
+      flash[:error] = "There was an error saving the question. Please try again."
+      render :edit
+    end 
+  end
+
+  def resolved
+    @question = Question.check_box
+    flash[:notice] = "This question has been resolved!"  
+
+  end
+
+  def destroy
+    @question = Question.delete
+    flash[:notice] = "The question was deleted."
+  end
+end
