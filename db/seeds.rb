fb2b405ab8557b24009500ad8eebc2cc46a13c9a
Jamaleh <Jamaleh@hotmail.com>
2015-06-16 13:56:50 +1000

    cp 38 progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
 require 'faker'
+
+5.times do
+  user = User.new(
+    name:     Faker::Name.name,
+    email:    Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+  )
+  user.skip_confirmation!
+  user.save!
+end 
+users = User.all
+
  
 50.times do 
   Post.create!(
+    user:   users.sample,
     title:  Faker::Lorem.sentence,
     body:   Faker::Lorem.paragraph
   )
@@ -10,6 +23,7 @@ posts = Post.all
  
 100.times do
   Comment.create!(
+    #users: users.sample,  #we have not yet associated Users with Comments.
     post: posts.sample,
     body: Faker::Lorem.paragraph
   )
@@ -27,6 +41,13 @@ unless comment
   Comment.create(body: "Some long body here")
 end 
  
+user = User.first
+user.skip_reconfirmation!
+user.update_attributes!(
+  email: 'jamalehgyulay@gmail.com',
+  password: 'brahma19'
+)
+
 puts "Seed finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
