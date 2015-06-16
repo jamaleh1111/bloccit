077279734ccfb97c1eccafbb9e0836ecdab550b1
Jamaleh <Jamaleh@hotmail.com>
2015-06-16 16:48:47 +1000

    cp 39 progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150612063907) do
+ActiveRecord::Schema.define(version: 20150616060856) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string   "title"
@@ -44,7 +44,10 @@ ActiveRecord::Schema.define(version: 20150612063907) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -72,6 +75,7 @@ ActiveRecord::Schema.define(version: 20150612063907) do
     t.string   "unconfirmed_email"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "role"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
