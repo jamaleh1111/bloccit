847f50a76be5fe49c7f456d6a65560d6c1a83c2d
Jamaleh <Jamaleh@hotmail.com>
2015-06-17 14:42:29 +1000

    progress on assnt 39

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,4 +1,15 @@
 class PostPolicy < ApplicationPolicy
+  class Scope 
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end 
+
+    def resolve
+      if user.admin?
+        scope.all? { ||  }
   def index?
     true
   end 
