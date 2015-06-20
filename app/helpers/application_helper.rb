416f47ce086c54e8748a4470ffebc043b3547d27
Jamaleh <Jamaleh@hotmail.com>
2015-06-20 16:53:49 +1000

    cp 42 complete

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,14 @@
 module ApplicationHelper
 
+  def form_group_tag(errors, &block)
+    if errors.any?
+      content_tag :div, capture(&block), class: 'form-group has-error'
+    else 
+      content_tag :div, capture(&block), class: 'form-group'
+    end 
+  end 
+
+
   def my_name
     my_name = "Jamaleh Gyulay"
   end 
