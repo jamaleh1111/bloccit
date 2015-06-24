0c0f75b0289eeace14575d62044c9556d762b486
Jamaleh <Jamaleh@hotmail.com>
2015-06-24 11:59:49 +1000

    complete a-44

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,7 @@ class PostsController < ApplicationController
   def create
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    @post.topic = @topic
     authorize @post 
     
     if @post.save
@@ -49,7 +49,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :body, :image)
+    params.require(:post).permit(:title, :body, :image, :image_cache)
   end 
 end 
 
