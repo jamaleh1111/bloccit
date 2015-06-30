7bd8a3aa0527549f64e832ade558cb2baa555295
Jamaleh <Jamaleh@hotmail.com>
2015-06-30 19:34:38 +1000

    cp mocking complete

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+  include TestFactories
+
+
 describe Vote do 
   
   describe "validations" do
@@ -14,6 +17,15 @@ describe Vote do
         invalid_vote = Vote.new(value: 2)
         expect(invalid_vote.valid?).to eq(false)
       end 
+    end 
+  end 
+
+  describe 'after_save' do
+    it "calls `Post#update_rank` after save" do
+      post = associated_post
+      vote = Vote.new(value: 1, post: post)
+      expect(post).to receive(:update_rank)
+      vote.save
     end 
   end 
 end 
