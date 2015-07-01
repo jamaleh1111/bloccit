453f84a45e9c003a04697274738d066137ee2beb
Jamaleh <Jamaleh@hotmail.com>
2015-07-01 22:43:22 +1000

    progress on cp testing users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,4 +22,31 @@ describe User do
     end
   end
 
+  describe ".top_rated" do 
+
+    before do
+      @user1 = create(:user)
+      post = create(:post, user: @user1)
+      create(:comment, user: @user1, post: post)
+
+      @user2 = create(:user)
+      post = create(:post, user: @user2)
+      2.times { create(:comment, user: @user2, post: post) }
+    end 
+
+    it "returns users ordered by comments + posts" do 
+      expect( User.top_rated ).to eq([@user2, @user1])
+    end 
+
+    it "stores a `post_count` on user" do 
+      users = User.top_rates
+      expect( users.first.posts_count ).to eq(1)
+    end 
+
+    it "stores a `comments_count` on user" do 
+      users = User.top_rates 
+      expect( users.first.comments_count ).to eq(2)
+    end 
+  end
+
 end 
