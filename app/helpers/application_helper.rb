fed6c5344abddecec143f12e54657cc546b32017
Jamaleh <Jamaleh@hotmail.com>
2015-07-02 15:14:42 +1000

    complete another-interlude-assignment

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,11 @@ module ApplicationHelper
     my_name = "Jamaleh Gyulay"
   end 
 
-  def up_vote_link_classes
+  def up_vote_link_classes(post)
     "glyphicon glyphicon-chevron-up #{(current_user.voted(post) && current_user.voted(post).up_vote?) ? 'voted' : ' ' }"  
   end 
 
-  def down_vote_link_classes
+  def down_vote_link_classes(post)
     "glyphicon glyphicon-chevron-down #{(current_user.voted(post) && current_user.voted(post).down_vote?) ? 'voted' : ' ' }"  
   end 
   
