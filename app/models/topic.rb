614d55767dfe434ef15db5de5f6a9ef9d07c80e7
Jamaleh <Jamaleh@hotmail.com>
2015-07-02 15:00:45 +1000

    progress on private topic assignment

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,9 +2,9 @@ class Topic < ActiveRecord::Base
 
   has_many :posts, dependent: :destroy
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
-  scope :publicly_viewable, -> { visible_to.where() }
-  scope :privately_viewable, -> { visible_to.where() }
+  scope :visible_to, -> (user) { user ? all : publicly_viewable }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
 
 
   validates :name, length: { minimum: 5 }, presence: true
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -21,12 +21,12 @@ describe Topic do
     describe "visible_to(user)" do
       it "returns all topics if the user is present" do
         user = true 
-        expect(publicly_viewable, privately_viewable).to eq( [@public_topic, @private_topic] )
+        expect(Topic.visible_to(user).to eq( [@public_topic, @private_topic] )
       end
 
       it "returns only public topics if user is nil" do
         user = false
-        expect(publicly_viewable).to eq( [@public_topic] )
+        expect(Topic.visible_to(user).to eq( [@public_topic] )
       end 
     end 
   end 
