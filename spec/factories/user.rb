578242b2562baa45c8313cfc4d5f8a0b44e3c48d
Jamaleh <Jamaleh@hotmail.com>
2015-07-02 16:44:55 +1000

    fix factories_user.rb

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -8,17 +8,18 @@ FactoryGirl.define do
   end 
 end 
 
- 
-  factory :user do
-    email 'test@example.com'
-    password 'f4k3p455w0rd'
-    user = FactoryGirl.create(:user)
-    user.confirmed_at = Time.now
-    user.save
+# FactoryGirl.define do
+#   factory :user do
+#     email 'test@example.com'
+#     password 'f4k3p455w0rd'
+#     user = FactoryGirl.create(:user)
+#     user.confirmed_at = Time.now
+#     user.save
   
-    RSpec.configure do |config|
-      config.after :each do
-        Warden.test_reset!
-      end
-    end 
-  end
+#     RSpec.configure do |config|
+#       config.after :each do
+#         Warden.test_reset!
+#       end
+#     end 
+#   end
+# end 
