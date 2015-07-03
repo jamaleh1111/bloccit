c5b534a91debc5c3f5a29c458a9c4bece2e164d6
Jamaleh <Jamaleh@hotmail.com>
2015-07-03 17:35:24 +1000

    complete bloccit

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -23,7 +23,7 @@ class Topics::PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
     @post.topic = @topic
-    @post = Post.new(params.require(:post).permit(:title, :body))
+    
     authorize @post 
     
     if @post.save
