163842e9cb47ae246e10da6f554dfc61ac39c182
Jamaleh <Jamaleh@hotmail.com>
2015-08-25 15:00:15 +1000

    fix some layout and post destroy

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -62,7 +62,7 @@ class Topics::PostsController < ApplicationController
     authorize @post 
     
     if @post.destroy
-      flash[:notice] = "\"#{post.title}\" was deleted successfully."
+      flash[:notice] = "Post was deleted successfully."
       redirect_to @topic
     else
       flash[:error] = "There was an error deleting the post. Please try again."
