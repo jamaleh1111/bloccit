52867d865daf4fc83d0d629a4efc520e1b949c92
Jamaleh <Jamaleh@hotmail.com>
2015-10-09 15:24:58 +1100

    working on image

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -26,26 +26,26 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
-  process :resize_to_fill => [200, 300]
+  process :resize_to_fit => [200, 200]
   # def scale(width, height)
   #   # do something
   # end
 
   # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :resize_to_fit => [50, 50]
-  # end
-  version :tiny do
-    process resize_to_fill: [20, 20]
-  end 
+  version :thumb do
+    process :resize_to_fill => [50, 50]
+  end
+  # version :tiny do
+  #   process resize_to_fill: [20, 20]
+  # end 
 
-  version :small do
-    process resize_to_fill: [30, 30]
-  end 
+  # version :small do
+  #   process resize_to_fill: [30, 30]
+  # end 
 
-  version :profile do
-    process resize_to_fill: [45, 45]
-  end 
+  # version :profile do
+  #   process resize_to_fill: [45, 45]
+  # end 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
